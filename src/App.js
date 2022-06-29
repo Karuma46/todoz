01dea14b7fb06a1ba8d738db9aad27b6a16cd625
steve karuma <stevekaruma@gmail.com>
2022-06-29 21:33:50 +0300

    Added context prodivers to app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { PromptContextProvider } from 'components/prompt/context';
+import { TasksContextProvider } from 'pages/context';
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from './pages/home';
@@ -5,11 +7,15 @@ import Home from './pages/home';
 const App = () => {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </Router>
+      <TasksContextProvider>
+        <PromptContextProvider>
+          <Router>
+            <Routes>
+              <Route path="*" element={<Home />} />
+            </Routes>
+          </Router>
+        </PromptContextProvider>
+      </TasksContextProvider>
     </div>
   );
 }
